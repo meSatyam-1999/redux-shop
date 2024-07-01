f52c2721fb8afd7c3c43222f1da7f5b635e5eca1
Satyam Singh <mesatyamsingh1999@gmail.com>
2024-07-01 10:05:46 +0530

    Navbar base setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <span className='logo'>Redux Shop</span>
+
+        <div>
+            <Link className='navlink' to='/'>Home</Link>
+            <Link className='navLink' to='/cart'>Cart</Link>
+            <span className='cartCount'>Cart Items: 0</span>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
